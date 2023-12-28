4b6b32749b17ca877f704dc9be978a8c798aa6ef
hydaline-code <djougancharlen@gmail.com>
2023-12-28 19:00:44 +0100

    Add new form post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,23 @@ class PostsController < ApplicationController
   def index
   end 
 
-  def create
+  def new
+    @post = Post.new
   end
-  
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save 
+      redirect_to  @post
+    else
+      render 'new'
+    end
+ end 
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
